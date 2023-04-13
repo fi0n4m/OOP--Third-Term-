62bf704ac35290471f3400bb09d7e8bbd47630d2
fi0n4m <111779860+fi0n4m@users.noreply.github.com>
2023-04-13 21:43:49 +0800

    Create EBookRental.java

---
+++ OOP 3rd Term/EBookRental.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EbookRental {
+
+    public static void main(String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    
+
+        System.out.println("\nELECTRONIC BOOK RENTAL SYSTEM");
+        System.out.println("------------------------------");
+        System.out.println(" 0 - System Analyst and Design [Gary B. Shelly]");
+        System.out.println(" 1 - Android Application [Corinne Hoisington]");
+        System.out.println(" 2 - Programming Concepts and Logic formulation [Rosauro E. Manuel]");
+        System.out.println("------------------------------");
+        System.out.println("\nPick the number of the book you want to borrow<3:"); 
+
+ }
+
+
+    }
